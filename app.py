b83de59882d308946cc43e1088f6ba7121c8a8d7
alltimessportsoficial <alltimessportsoficial@gmail.com>
2026-06-22 20:43:43 -0300

    teste post 2

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "All Times Sports - Copa 2026 API Online!"
+
 @app.route("/gerar", methods=["POST"])
 def gerar():
     print("POST RECEBIDO")
-
     dados = request.json
 
     return jsonify({
         "status": "ok",
         "dados": dados
     })
+
+if __name__ == "__main__":
+    app.run()
